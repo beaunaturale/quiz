7b3171aea9b5673745e437964d26576df9abfe64
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-03-19 21:18:36 -0500

    first commit, issue with evnt listener

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+var mainEl = document.querySelector('main');
+var playBtnEl = document.querySelector('#play-btn');
+var timerEl = document.querySelector('#time-el');
+
+var interval;
+var time = 100;
+
+
+playBtnEl.addEventListener('click', function (event) {
+  console.log('hit')
+
+});
+
